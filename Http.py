5dead6ae182caaac1988654c1d342b81ff01df46
Tom van meer <tom.vmeer@gmail.com>
2017-09-03 17:57:23 +0200

    Added stuff

--- Http.py
+++ Http.py
@@ -1,13 +1,27 @@
 import urllib2
-data = urllib2.urlopen("https://nl.wikipedia.org/wiki/Vissen_(dieren)").read()
-urls = []
-for i in range(1,len(data.split("href="))):
-    if "http" in ((data.split("href="))[i]):
-        try:
-            urls.append("http://"+(((data.split("href="))[i]).split('"')[1]).split("//")[1])
-        except:
-            pass
-f = open("urls.txt","w")
+
+def goto(link,urls):
+    data = urllib2.urlopen(link).read()
+    urls.append("outgoing from:"+str(link))
+    for i in range(1,len(data.split("href="))):
+        if "http" in ((data.split("href="))[i]):
+            try:
+                url = ("http://"+(((data.split("href="))[i]).split('"')[1]).split("//")[1])
+                if url not in urls:
+                    urls.append(url)
+            except:
+                pass
+    return urls
+
+urls = goto("http://nl.wikipedia.org/w/index.php?title=Vissen_(dieren)&amp;oldid=49574769",[])
 for i in urls:
-    f.write(i+"\n")
-f.close()
+    try:
+        goto(i,urls)
+        f = open("urls.txt","w")
+        for url in urls:
+            f.write(str(url)+"\n")
+        f.close()
+        print(len(urls))
+    except:
+        pass
+
